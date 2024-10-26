c0e1711344bca46e4f6a20925317b2eea2bccb79
R-saumya <124419998+R-saumya@users.noreply.github.com>
2024-10-26 20:12:48 +0530

    initial commit

---
+++ src/main/java/com/report/parser/ReportParserApplication.java
@@ -0,0 +1,13 @@
+package com.report.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/report/parser/ReportParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.report.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
